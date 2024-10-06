9462ab89958e91e533eb8f8819b028d63705e0b4
Emmanuel Gautier <git@emmanuelgautier.fr>
2024-10-06 15:20:26 +0200

    fix: keep jwt claims in the same order

--- jwt/hmac_alg.go
+++ jwt/hmac_alg.go
@@ -3,6 +3,6 @@ package jwt
 import jwtlib "github.com/golang-jwt/jwt/v5"
 
 func (j *JWTWriter) IsHMACAlg() bool {
-	_, ok := j.Token.Method.(*jwtlib.SigningMethodHMAC)
+	_, ok := j.GetToken().Method.(*jwtlib.SigningMethodHMAC)
 	return ok
 }
