028eaafb049905033f7f31c4825f7fac857814b2
Emmanuel Gautier <git@emmanuelgautier.fr>
2024-10-30 23:51:09 +0100

    feat: improve graphql introspection scan

--- scan/graphql/introspection_enabled/introspection_enabled_test.go
+++ scan/graphql/introspection_enabled/introspection_enabled_test.go
@@ -18,7 +18,7 @@ func TestGraphqlIntrospectionScanHandler_Failed_WhenRespondHTTPStatusIsOK(t *tes
 	httpmock.ActivateNonDefault(client.Client)
 	defer httpmock.DeactivateAndReset()
 
-	operation := operation.MustNewOperation(http.MethodGet, "http://localhost:8080/", nil, client)
+	operation := operation.MustNewOperation(http.MethodPost, "http://localhost:8080/", nil, client)
 	resBody := []byte(`{"data": {"__schema": {"queryType": {"name": "Query"}}}}`)
 	httpmock.RegisterResponder(http.MethodPost, operation.URL.String(), httpmock.NewBytesResponder(http.StatusOK, resBody))
 	httpmock.RegisterResponder(http.MethodGet, operation.URL.String(), httpmock.NewBytesResponder(http.StatusOK, resBody))
@@ -30,14 +30,47 @@ func TestGraphqlIntrospectionScanHandler_Failed_WhenRespondHTTPStatusIsOK(t *tes
 	assert.True(t, report.Issues[0].HasFailed())
 }
 
-func TestGraphqlIntrospectionScanHandler_Passed_WhenNotFoundStatus(t *testing.T) {
+func TestGraphqlIntrospectionScanHandler_Failed_WhenRespond_GETMethodOnly_HTTPStatusIsOK(t *testing.T) {
 	client := request.GetDefaultClient()
 	httpmock.ActivateNonDefault(client.Client)
 	defer httpmock.DeactivateAndReset()
 
-	operation := operation.MustNewOperation(http.MethodGet, "http://localhost:8080/", nil, client)
-	httpmock.RegisterResponder(http.MethodPost, operation.URL.String(), httpmock.NewBytesResponder(http.StatusNoContent, nil))
-	httpmock.RegisterResponder(http.MethodGet, operation.URL.String(), httpmock.NewBytesResponder(http.StatusNoContent, nil))
+	operation := operation.MustNewOperation(http.MethodPost, "http://localhost:8080/", nil, client)
+	resBody := []byte(`{"data": {"__schema": {"queryType": {"name": "Query"}}}}`)
+	httpmock.RegisterResponder(http.MethodPost, operation.URL.String(), httpmock.NewBytesResponder(http.StatusBadRequest, nil))
+	httpmock.RegisterResponder(http.MethodGet, operation.URL.String(), httpmock.NewBytesResponder(http.StatusOK, resBody))
+
+	report, err := introspectionenabled.ScanHandler(operation, auth.NewNoAuthSecurityScheme())
+
+	require.NoError(t, err)
+	assert.Equal(t, 2, httpmock.GetTotalCallCount())
+	assert.True(t, report.Issues[0].HasFailed())
+}
+
+func TestGraphqlIntrospectionScanHandler_Passed_WhenBadRequestStatus(t *testing.T) {
+	client := request.GetDefaultClient()
+	httpmock.ActivateNonDefault(client.Client)
+	defer httpmock.DeactivateAndReset()
+
+	operation := operation.MustNewOperation(http.MethodPost, "http://localhost:8080/", nil, client)
+	httpmock.RegisterResponder(http.MethodPost, operation.URL.String(), httpmock.NewBytesResponder(http.StatusBadRequest, nil))
+	httpmock.RegisterResponder(http.MethodGet, operation.URL.String(), httpmock.NewBytesResponder(http.StatusBadRequest, nil))
+
+	report, err := introspectionenabled.ScanHandler(operation, auth.NewNoAuthSecurityScheme())
+
+	require.NoError(t, err)
+	assert.Equal(t, 2, httpmock.GetTotalCallCount())
+	assert.True(t, report.Issues[0].HasPassed())
+}
+
+func TestGraphqlIntrospectionScanHandler_Passed_WhenOKStatusButNoQuery(t *testing.T) {
+	client := request.GetDefaultClient()
+	httpmock.ActivateNonDefault(client.Client)
+	defer httpmock.DeactivateAndReset()
+
+	operation := operation.MustNewOperation(http.MethodPost, "http://localhost:8080/", nil, client)
+	httpmock.RegisterResponder(http.MethodPost, operation.URL.String(), httpmock.NewBytesResponder(http.StatusOK, nil))
+	httpmock.RegisterResponder(http.MethodGet, operation.URL.String(), httpmock.NewBytesResponder(http.StatusOK, nil))
 
 	report, err := introspectionenabled.ScanHandler(operation, auth.NewNoAuthSecurityScheme())
 
